0da2d48abf2841ffff550003b56a1e07eaec0865
Wolfgang Sourdeau <wolfgang@datacratic.com>
2013-05-07 09:52:13 -0400

    added test for timer handling

--- service/testing/endpoint_unit_test.cc
+++ service/testing/endpoint_unit_test.cc
@@ -8,7 +8,10 @@
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 
+#include <sys/eventfd.h>
+
 #include <boost/test/unit_test.hpp>
+#include "soa/service/endpoint.h"
 #include "soa/service/http_endpoint.h"
 #include "soa/service/active_endpoint.h"
 #include "soa/service/passive_endpoint.h"
@@ -85,7 +88,6 @@ BOOST_AUTO_TEST_CASE( test_active_endpoint_no_threads )
 }
 #endif
 
-
 BOOST_AUTO_TEST_CASE( test_passive_endpoint )
 {
     BOOST_REQUIRE_EQUAL(TransportBase::created, TransportBase::destroyed);
@@ -106,4 +108,72 @@ BOOST_AUTO_TEST_CASE( test_passive_endpoint )
     BOOST_CHECK_EQUAL(TransportBase::created, TransportBase::destroyed);
     BOOST_CHECK_EQUAL(ConnectionHandler::created,
                       ConnectionHandler::destroyed);
+}
+
+namespace {
+
+struct MockEndpoint : public Datacratic::EndpointBase {
+    MockEndpoint (const std::string & name)
+        : EndpointBase(name)
+    {
+    }
+
+    virtual std::string hostname()
+        const
+    {
+        return "mock-ep";
+    }
+    
+    virtual int port()
+        const
+    {
+        return -1;
+    }
+
+    virtual void closePeer()
+    {}
+};
+
+}
+
+BOOST_AUTO_TEST_CASE( test_EndpointBase_handleTimerEvent )
+{
+    /* FIXME: this test currenly ensure that the thread assignment mechanism
+       works */
+    Watchdog watchdog(5.0);
+
+    int timerFd = eventfd(0, 0);
+    uint64_t eventData(12345);
+    int x = ::write(timerFd, &eventData, sizeof(eventData));
+    BOOST_CHECK_EQUAL(x, sizeof(eventData));
+
+    EndpointBase::EpollData timerData(EndpointBase::EpollData::TIMER, timerFd);
+
+    int timerInvoked(0);
+    uint64_t lastNums(0);
+    timerData.onTimer = [&] (uint64_t nums) {
+        timerInvoked++;
+        lastNums = nums;
+    };
+
+    MockEndpoint anEndpoint("myep");
+    bool rc = anEndpoint.handleTimerEvent(timerData);
+
+    /* must have invoked timer */
+    BOOST_CHECK_EQUAL(timerInvoked, 1);
+    BOOST_CHECK_EQUAL(lastNums, 12345);
+    BOOST_CHECK_EQUAL(rc, false);
+    BOOST_CHECK_NE(timerData.threadId, 0); /* thread id must be assigned */
+
+    pid_t threadId = timerData.threadId;
+    timerData.threadId = threadId^0xffffffff;
+    eventData = 54321;
+    x = ::write(timerFd, &eventData, sizeof(eventData));
+    BOOST_CHECK_EQUAL(x, sizeof(eventData));
+    rc = anEndpoint.handleTimerEvent(timerData);
+    /* must not have invoked timer */
+    BOOST_CHECK_EQUAL(timerInvoked, 1);
+    BOOST_CHECK_EQUAL(lastNums, 12345);
+    BOOST_CHECK_EQUAL(rc, true);
+    BOOST_CHECK_NE(timerData.threadId, threadId);
 }
