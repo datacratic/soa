183fe0bb1ead73a12e49d1ffa9d7e7ea4c2322c4
Rémi Attab <remi@datacratic.com>
2013-10-16 13:48:10 -0400

    Endpoint can now spin up or spin down threads depending on the load.

--- service/endpoint.h
+++ service/endpoint.h
@@ -92,7 +92,7 @@ struct EndpointBase : public Epoller {
     /** Total number of seconds that this message loop has spent sleeping.
         Can be polled regularly to determine the duty cycle of the loop.
      */
-    std::vector<double> totalSleepSeconds() const { return totalSleepTime; }
+    std::vector<double> totalWorkSeconds() const { return totalWorkTime; }
 
     /** Thing to notify when a connection is closed.  Will be called
         before the normal cleanup.
@@ -243,7 +243,7 @@ private:
 
     std::map<std::string, int> numTransportsByHost;
 
-    std::vector<double> totalSleepTime;
+    std::vector<double> totalWorkTime;
 
     /** Run a thread to handle events. */
     void runEventThread(int threadNum, int numThreads);
