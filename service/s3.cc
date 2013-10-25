5c8d9ec3b1cb2f455d55f9caeefea876ed5a72c3
Wolfgang Sourdeau <wolfgang@datacratic.com>
2013-10-24 23:21:08 -0400

    fixed bug induced by typo

--- service/s3.cc
+++ service/s3.cc
@@ -1516,7 +1516,7 @@ struct StreamingDownloadSource {
                 //cerr << "downloading" << endl;
 
                 auto partResult
-                    = owner->get(bucket, "/" + object, end - start);
+                    = owner->get(bucket, "/" + object, Range(start, end - start));
                 if (partResult.code_ != 206) {
                     cerr << "error getting part "
                          << partResult.bodyXmlStr() << endl;
