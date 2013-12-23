10f2bf14322886f31289e7ef76cff123b3e4dad6
Jeremy Barnes <jeremy@datacratic.com>
2013-12-23 16:24:42 -0500

    merged utils, arch, compiler, db, stats from jml into soa

--- utils/testing/testing.mk
+++ utils/testing/testing.mk
@@ -9,3 +9,14 @@
 $(eval $(call test,fixture_test,test_utils,boost))
 $(eval $(call test,variadic_hash_test,variadic_hash,boost))
 
+$(eval $(call test,parse_context_test,utils arch,boost))
+$(eval $(call test,configuration_test,utils arch,boost))
+$(eval $(call test,environment_test,utils arch,boost))
+$(eval $(call test,compact_vector_test,arch,boost))
+$(eval $(call test,circular_buffer_test,arch,boost))
+$(eval $(call test,lightweight_hash_test,arch utils,boost))
+$(eval $(call test,filter_streams_test,arch utils boost_filesystem boost_system,boost))
+$(eval $(call test,csv_parsing_test,arch utils,boost))
+
+$(eval $(call test,worker_task_test,worker_task ACE arch boost_thread pthread,boost manual))
+$(eval $(call test,json_parsing_test,utils arch,boost))
--- utils/utils.mk
+++ utils/utils.mk
@@ -6,6 +6,33 @@
 # Makefile of soa's misc utilities.
 #------------------------------------------------------------------------------#
 
+LIBUTILS_SOURCES := \
+        environment.cc \
+        file_functions.cc \
+        filter_streams.cc \
+        string_functions.cc \
+        parse_context.cc \
+	configuration.cc \
+	csv.cc \
+	exc_check.cc \
+	exc_assert.cc \
+	hex_dump.cc \
+	lzma.cc \
+	floating_point.cc \
+	json_parsing.cc \
+	rng.cc \
+	hash.cc \
+	abort.cc
+
+LIBUTILS_LINK :=	arch boost_iostreams lzma boost_thread cryptopp
+
+$(eval $(call library,utils,$(LIBUTILS_SOURCES),$(LIBUTILS_LINK)))
+
+LIBWORKER_TASK_SOURCES := worker_task.cc
+LIBWORKER_TASK_LINK    := arch pthread
+
+$(eval $(call library,worker_task,$(LIBWORKER_TASK_SOURCES),$(LIBWORKER_TASK_LINK)))
+
 
 LIB_TEST_UTILS_SOURCES := \
         fixtures.cc \
