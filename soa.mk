1f122c659aec8046f5c5e2b973cbcc68724efac6
Jeremy Barnes <jeremy@datacratic.com>
2013-12-23 16:29:25 -0500

    added in judy and math

--- soa.mk
+++ soa.mk
@@ -1,5 +1,12 @@
 # SOA makefile
 
+$(eval $(call include_sub_make,math))
+$(eval $(call include_sub_make,arch))
+$(eval $(call include_sub_make,utils))
+$(eval $(call include_sub_make,db))
+$(eval $(call include_sub_make,stats))
+$(eval $(call include_sub_make,judy))
+
 $(eval $(call include_sub_make,jsoncpp))
 $(eval $(call include_sub_make,types))
 $(eval $(call include_sub_make,js))
@@ -10,3 +17,5 @@ $(eval $(call include_sub_make,service))
 $(eval $(call include_sub_make,logger))
 $(eval $(call include_sub_make,launcher))
 $(eval $(call include_sub_make,utils))
+
+
