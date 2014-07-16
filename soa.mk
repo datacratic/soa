8400533ec8d5bb26e1d7b63bc737e3cf4f37e7b0
Wolfgang Sourdeau <wolfgang@datacratic.com>
2014-07-16 13:05:49 -0400

    libtest_utils built sooner

--- soa.mk
+++ soa.mk
@@ -2,6 +2,7 @@
 
 $(eval $(call include_sub_make,jsoncpp))
 $(eval $(call include_sub_make,types))
+$(eval $(call include_sub_make,utils))
 $(eval $(call include_sub_make,js))
 $(eval $(call include_sub_make,sync))
 $(eval $(call include_sub_make,sigslot))
@@ -9,5 +10,4 @@ $(eval $(call include_sub_make,gc))
 $(eval $(call include_sub_make,service))
 $(eval $(call include_sub_make,logger))
 $(eval $(call include_sub_make,launcher))
-$(eval $(call include_sub_make,utils))
 $(eval $(call include_sub_make,pipeline))
