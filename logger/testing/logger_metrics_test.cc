cdb9221d17b82bb2a413f990dcd767dbc5b8c7b7
Mich <fmlheureux@datacratic.com>
2014-12-16 16:55:12 -0500

    Improved mongo test with mongo server

--- logger/testing/logger_metrics_test.cc
+++ logger/testing/logger_metrics_test.cc
@@ -17,14 +17,50 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/regex.hpp>
 
+#include "mongo/bson/bson.h"
+#include "mongo/bson/bsonobj.h"
+#include "mongo/client/dbclient.h"
+#include "jml/utils/filter_streams.h"
+#include "soa/service/testing/mongo_temporary_server.h"
 #include "soa/logger/logger_metrics_interface.h"
 
+using namespace ML;
 using namespace Datacratic;
 using namespace std;
 
 BOOST_AUTO_TEST_CASE( test_logger_metrics ) {
+    Mongo::MongoTemporaryServer mongo;
+    setenv("CONFIG", "logger/testing/logger_metrics_config.json", 1);
     shared_ptr<ILoggerMetrics> logger =
         ILoggerMetrics::setup("metricsLogger", "lalmetrics", "test");
 
     logger->logMeta({"a", "b"}, "taratapom");
+
+    Json::Value config;
+    filter_istream cfgStream("logger/testing/logger_metrics_config.json");
+    cfgStream >> config;
+
+    Json::Value metricsLogger = config["metricsLogger"];
+    auto conn = std::make_shared<mongo::DBClientConnection>();
+    conn->connect(metricsLogger["hostAndPort"].asString());
+    string err;
+    if (!conn->auth(metricsLogger["database"].asString(),
+                    metricsLogger["user"].asString(),
+                    metricsLogger["pwd"].asString(), err)) {
+        throw ML::Exception("Failed to log to mongo tmp server: %s",
+                            err.c_str());
+    }
+
+    BOOST_CHECK_EQUAL(conn->count("test.lalmetrics"), 1);
+    auto cursor = conn->query("test.lalmetrics", mongo::BSONObj());
+    if (cursor->more()) {
+        mongo::BSONObj p = cursor->next();
+        cerr << p.toString() << endl;
+        cerr << "FM:" << p["meta"]["a"]["b"].String() << endl;
+        cerr << "FM:" << p["meta"]["a"]["b"].String() << endl;
+        BOOST_CHECK_EQUAL(p.getFieldDotted("meta.a.b").String(), "taratapom");
+    }
+    else {
+        throw ML::Exception("Nothing to read");
+    }
 }
