b5897f796f7fc3b0ac744f1607809c33218ca6f8
Wolfgang Sourdeau <Wolfgang@Contre.COM>
2015-03-06 16:10:13 -0500

    MLDB-99: added HDFS write support

--- service/hdfs.cc
+++ service/hdfs.cc
@@ -1,6 +1,6 @@
 /* hdfs.cc
-   Wolfgang Sourdeau, January 2014
-   Copyright (c) 2014 Datacratic.  All rights reserved.
+   Wolfgang Sourdeau, March 2015
+   Copyright (c) 2015 Datacratic.  All rights reserved.
 */
 
 #include <memory>
@@ -23,12 +23,128 @@ constexpr int DEFAULT_PORT(50070);
 
 namespace {
 
-struct HDFSStreamingDownloadSource {
-    HDFSStreamingDownloadSource(const string & urlStr)
-    {
-        impl.reset(new Impl(urlStr));
+/****************************************************************************/
+/* HDFS SOURCE IMPL                                                         */
+/****************************************************************************/
+
+struct HDFSSourceImpl {
+    HDFSSourceImpl(const string & urlStr, int mode);
+    ~HDFSSourceImpl();
+
+    streamsize read(char * s, streamsize n);
+    streamsize write(const char* s, streamsize n);
+
+private:
+    void cleanup() noexcept;
+
+    int mode_;
+    hdfsFS fsHandle_;
+    hdfsFile fileHandle_;
+};
+
+HDFSSourceImpl::
+HDFSSourceImpl(const string & urlStr, int mode)
+    : mode_(mode)
+{
+    Url url(urlStr);
+
+    string hostname = url.host();
+    int port = url.port();
+    if (port == -1) {
+        port = DEFAULT_PORT;
+    }
+    string username = url.username();
+    string filename = url.path();
+
+    ExcAssertEqual(url.scheme(), "hdfs");
+    ExcAssert(!filename.empty());
+
+    hdfsBuilder * builder = hdfsNewBuilder();
+    string baseUrl = "hdfs://" + hostname + ":" + to_string(port);
+    hdfsBuilderSetNameNode(builder, baseUrl.c_str());
+
+    if (!username.empty()) {
+        hdfsBuilderSetUserName(builder, username.c_str());
     }
 
+    bool ok(false);
+    ML::Call_Guard guard([&] { if (!ok) cleanup(); });
+
+    fsHandle_ = hdfsBuilderConnect(builder);
+    ExcAssert(fsHandle_ != nullptr);
+
+    /* "hdfsExists" returns the opposite of what it means */
+    if ((mode & O_WRONLY) == 0 && hdfsExists(fsHandle_, filename.c_str())) {
+        throw ML::Exception("file does not exist");
+    }
+
+    fileHandle_ = hdfsOpenFile(fsHandle_, filename.c_str(), mode, 0,
+                               0, 0);
+    ExcAssert(fileHandle_ != nullptr);
+
+    ok = true;
+}
+
+HDFSSourceImpl::
+~HDFSSourceImpl()
+{
+    cleanup();
+}
+
+streamsize
+HDFSSourceImpl::
+read(char * s, streamsize n)
+{
+    ExcAssert((mode_ & O_RDONLY) == O_RDONLY);
+
+    tSize readRes;
+    {
+        JML_TRACE_EXCEPTIONS(false);
+        readRes = hdfsRead(fsHandle_, fileHandle_, s, n);
+    }
+    if (readRes == -1) {
+        throw ML::Exception(errno, "hdfsRead");
+    }
+
+    return readRes > 0 ? readRes : -1;
+}
+
+streamsize
+HDFSSourceImpl::
+write(const char* s, streamsize n)
+{
+    ExcAssert((mode_ & O_WRONLY) == O_WRONLY);
+
+    tSize writeRes = hdfsWrite(fsHandle_, fileHandle_, s, n);
+    if (writeRes == -1) {
+        throw ML::Exception(errno, "hdfsWrite");
+    }
+
+    return writeRes;
+}
+
+void
+HDFSSourceImpl::
+cleanup()
+    noexcept
+{
+    if (fileHandle_) {
+        hdfsCloseFile(fsHandle_, fileHandle_);
+        fileHandle_ = nullptr;
+    }
+    if (fsHandle_) {
+        hdfsDisconnect(fsHandle_);
+        fsHandle_ = nullptr;
+    }
+}
+
+
+/****************************************************************************/
+/* HDFS DL SOURCE                                                           */
+/****************************************************************************/
+
+/* Source for ML::filter_istream */
+struct HDFSDlSource {
     typedef char char_type;
     struct category
         : //input_seekable,
@@ -37,105 +153,88 @@ struct HDFSStreamingDownloadSource {
         boost::iostreams::closable_tag
     { };
 
-    struct Impl {
-        Impl(const string & urlStr)
-        {
-            Url url(urlStr);
-
-            string hostname = url.host();
-            int port = url.port();
-            if (port == -1) {
-                port = DEFAULT_PORT;
-            }
-            string username = url.username();
-            string filename = url.path();
-
-            ExcAssertEqual(url.scheme(), "hdfs");
-            ExcAssert(!filename.empty());
-
-            hdfsBuilder * builder = hdfsNewBuilder();
-            string baseUrl = "hdfs://" + hostname + ":" + to_string(port);
-            hdfsBuilderSetNameNode(builder, baseUrl.c_str());
-
-            if (!username.empty()) {
-                hdfsBuilderSetUserName(builder, username.c_str());
-            }
-
-            bool ok(false);
-            ML::Call_Guard guard([&] { if (!ok) cleanup(); });
-
-            fsHandle_ = hdfsBuilderConnect(builder);
-            ExcAssert(fsHandle_ != nullptr);
-
-            if (hdfsExists(fsHandle_, filename.c_str())) {
-                throw ML::Exception("file does not exist");
-            }
-
-            fileHandle_ = hdfsOpenFile(fsHandle_, filename.c_str(), O_RDONLY, 0,
-                                       0, 0);
-            ExcAssert(fileHandle_ != nullptr);
-
-            ok = true;
-        }
-
-        ~Impl()
-        {
-            cleanup();
-        }
-
-        void cleanup()
-            noexcept
-        {
-            if (fileHandle_) {
-                hdfsCloseFile(fsHandle_, fileHandle_);
-                fileHandle_ = nullptr;
-            }
-            if (fsHandle_) {
-                hdfsDisconnect(fsHandle_);
-                fsHandle_ = nullptr;
-            }
-        }
-
-        streamsize read(char_type * s, streamsize n)
-        {
-            tSize readRes;
-
-            {
-                JML_TRACE_EXCEPTIONS(false);
-                readRes = hdfsRead(fsHandle_, fileHandle_, s, n);
-            }
-            if (readRes == -1) {
-                throw ML::Exception(readRes, "hdfsRead");
-            }
-
-            return readRes > 0 ? readRes : -1;
-        }
-
-    private:
-        hdfsFS fsHandle_;
-        hdfsFile fileHandle_;
-        // tOffset fileSize_;
-        // tOffset offset_;
-    };
-
-    shared_ptr<Impl> impl;
-
-    streamsize read(char_type* s, streamsize n)
+    HDFSDlSource(const string & urlStr)
     {
-        return impl->read(s, n);
+        impl_.reset(new HDFSSourceImpl(urlStr, O_RDONLY));
+    }
+
+    streamsize read(char* s, streamsize n)
+    {
+        return impl_->read(s, n);
     }
 
     bool is_open() const
     {
-        return !!impl;
+        return !!impl_;
     }
 
     void close()
     {
-        impl.reset();
+        impl_.reset();
     }
+
+private:
+    shared_ptr<HDFSSourceImpl> impl_;
 };
 
+
+/****************************************************************************/
+/* HDFS UL SOURCE                                                           */
+/****************************************************************************/
+
+/* Source for ML::filter_ostream */
+
+struct HDFSUlSource {
+    typedef char char_type;
+    struct category
+        : public boost::iostreams::output,
+          public boost::iostreams::device_tag,
+          public boost::iostreams::closable_tag
+    {
+    };
+
+    HDFSUlSource(const string & urlStr,
+                 const ML::OnUriHandlerException & onException)
+        : onException_(onException)
+    {
+        try {
+            impl_.reset(new HDFSSourceImpl(urlStr, O_WRONLY));
+        }
+        catch (...) {
+            if (onException_) {
+                onException_();
+            }
+            throw;
+        }
+    }
+
+    streamsize write(const char * s, streamsize n)
+    {
+        try {
+            return impl_->write(s, n);
+        }
+        catch (...) {
+            if (onException_) {
+                onException_();
+            }
+            throw;
+        }
+    }
+
+    bool is_open() const
+    {
+        return !!impl_;
+    }
+
+    void close()
+    {
+        impl_.reset();
+    }
+
+private:
+    shared_ptr<HDFSSourceImpl> impl_;
+    ML::OnUriHandlerException onException_;
+};
 
 /** Register HDFS with the filter streams API.
 */
@@ -147,22 +246,25 @@ struct RegisterHDFSHandler {
                    const map<string, string> & options,
                    const ML::OnUriHandlerException & onException)
     {
+        streambuf * result;
+
         string::size_type pos = scheme.find("hdfs://");
         if (pos != string::npos)
             throw ML::Exception("malformed hdfs url");
         string url = "hdfs://" + resource;
 
         if (mode == ios::in) {
-            streambuf * result
-                = new boost::iostreams::stream_buffer<HDFSStreamingDownloadSource>(HDFSStreamingDownloadSource(url), 131072);
-            return make_pair(result, true);
+            result = new boost::iostreams::stream_buffer<HDFSDlSource>(HDFSDlSource(url), 131072);
         }
         else if (mode == ios::out) {
-            throw ML::Exception("stream writing not supported yet");
+             result =
+                 new boost::iostreams::stream_buffer<HDFSUlSource>(HDFSUlSource(url, onException),
+                                                                   131072);
         }
         else {
             throw ML::Exception("no way to create HDFS handler for non in/out");
         }
+        return make_pair(result, true);
     }
 
     RegisterHDFSHandler()
