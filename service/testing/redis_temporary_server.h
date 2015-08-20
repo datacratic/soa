5597a2367805632c083af88d53df9860bbf9207b
Antoine F Moevus <antoine@datacratic.com>
2015-08-20 12:05:39 -0400

    Improve creation of redis temp server's directory

--- service/testing/redis_temporary_server.h
+++ service/testing/redis_temporary_server.h
@@ -31,8 +31,10 @@ struct RedisTemporaryServer : boost::noncopyable {
         using namespace std;
         if (uniquePath == "") {
             ML::Env_Option<std::string> tmpDir("TMP", "./tmp");
+            std::string dir = tmpDir;
+            if(dir[0] == '/') dir.insert(0, 1, '.');
             uniquePath = ML::format("%s/redis-temporary-server-%d-%d",
-                                    tmpDir.get(), getpid(), index);
+                                    dir, getpid(), index);
             cerr << "starting redis temporary server under unique path "
                  << uniquePath << endl;
         }
