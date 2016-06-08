3dd179d64185584f26d3df400cad9034e56e926c
Wolfgang Sourdeau <wolfgang@datacratic.com>
2016-06-08 15:19:02 -0400

    LAL-1067: moved "log_metrics" out of module

--- logger/js/js.mk
+++
@@ -1,1 +0,0 @@
-$(eval $(call nodejs_addon,iloggermetricscpp,logger_metrics_interface_js.cc,js log_metrics,))
--- logger/testing/logger_testing.mk
+++ logger/testing/logger_testing.mk
@@ -9,7 +9,3 @@ $(eval $(call test,logger_deadlock_test,logger,boost manual))
 
 $(eval $(call test,multi_output_logger_test,logger,boost))
 $(eval $(call test,rotating_file_logger_test,logger,manual boost))
-
-$(eval $(call vowscoffee_test,logger_metrics_interface_js_test,iloggermetricscpp))
-
-$(eval $(call test,logger_metrics_test,log_metrics mongo_tmp_server utils,manual boost))
